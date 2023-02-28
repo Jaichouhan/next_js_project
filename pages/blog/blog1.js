04241ce735a64bed653cd7193cad3936f7e0d01f
Jai chouhan <92376924+Jaichouhan@users.noreply.github.com>
2023-02-28 12:40:39 +0530

    folder with file routing done

---
+++ pages/blog/blog1.js
@@ -0,0 +1,5 @@
+const blog1 = () => {
+  return <div>Hello Blogs1 done</div>;
+};
+
+export default blog1;
---
+++ pages/blog/blog2.js
@@ -0,0 +1,5 @@
+const blog2 = () => {
+  return <div>Hello Blogs2 done</div>;
+};
+
+export default blog2;
